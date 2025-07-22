8d81a9037b843694cdd7100dc3e880825dee0561
张帅 <47773565+254558@users.noreply.github.com>
2025-07-22 21:08:51 +0800

    hi

---
+++ 5.store-getters/src/dango-shop.js
@@ -0,0 +1,32 @@
+import { acceptHMRUpdate, defineStore } from 'pinia'
+
+export const useDangoShop = defineStore('store-getters', {
+  state: () => ({
+    amount: 0,
+  }),
+
+  getters: {
+    totalPrice: state => state.amount * DANGO_PRICE,
+
+    discountedPrice() {
+      if (this.amount < 3) return this.totalPrice
+      if (this.amount < 5) return Math.ceil(this.totalPrice * 0.9)
+      if (this.amount < 10) return Math.ceil(this.totalPrice * 0.85)
+      return Math.ceil(this.totalPrice * 0.8)
+    },
+
+    hasPriceDiscount: state => state.amount >= 3,
+
+    savedMoney() {
+      return this.totalPrice - this.discountedPrice
+    },
+  },
+})
+
+// Do not change this value
+const DANGO_PRICE = 350
+
+if (import.meta.hot) {
+  import.meta.hot.accept(acceptHMRUpdate(useDangoShop, import.meta.hot))
+}
+    
